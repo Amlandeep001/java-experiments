8c3c5390db3593ed457d0b5ab98fd172446907f3
Amlan Deep Nandi <99658242+Amlandeep001@users.noreply.github.com>
2024-09-20 02:25:16 +0530

    Updated the java file leetcode link

--- LeetCodeProblems/src/com/org/codingninjas/CountFrequency.java
+++ LeetCodeProblems/src/com/org/codingninjas/CountFrequency.java
@@ -1,5 +1,7 @@
 package com.org.codingninjas;
 
+//https://www.naukri.com/code360/problems/count-frequency_920323?interviewProblemRedirection=true&page=2&practice_topic%5B%5D=Strings&difficulty%5B%5D=Easy&leftPanelTabValue=PROBLEM
+
 public class CountFrequency
 {
     public static int[] findFrequency(String S)
