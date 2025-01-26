6039cd2e42a5676d585d0d0c384f1f00e295e90f
Amlan Deep Nandi <99658242+Amlandeep001@users.noreply.github.com>
2025-01-26 21:33:02 +0530

    Updated for Virtual Threads

---
+++ expriments-with-java/src/com/org/expriments/threads/MultiThreadDemo.java
@@ -0,0 +1,21 @@
+package com.org.expriments.threads;
+
+/**
+ * @author Amlan
+ * @implNote: creation of threads via extending the Thread class
+ */
+public class MultiThreadDemo extends Thread
+{
+    @Override
+    public void run()
+    {
+        try
+        {
+            System.out.println("Current Thread: " + Thread.currentThread().getId() + " is running");
+        }
+        catch (Exception e)
+        {
+            System.out.println("Exception occured");
+        }
+    }
+}
---
+++ expriments-with-java/src/com/org/expriments/threads/SampleVirtualThreads.java
@@ -0,0 +1,57 @@
+package com.org.expriments.threads;
+
+import java.util.concurrent.Executors;
+
+/**
+ * 1. Lightweight Threads
+Virtual Threads are much lighter than traditional threads, enabling you to handle thousands or even millions of concurrent tasks without running into memory or performance issues.
+
+ 2. Simplified Concurrency
+Forget about complex thread pool management. Virtual Threads make it easier to write and maintain concurrent code.
+
+ 3. Improved Scalability
+Virtual Threads are designed for scalable applications, making them perfect for building high-performance web servers or handling large-scale asynchronous tasks.
+
+ 4. Reduced Context Switching
+Virtual Threads minimize the cost of context switching, which improves overall application performance.
+
+ 5. Better Resource Utilization
+Virtual Threads reduce the memory overhead of traditional threads, allowing you to make better use of system resources.
+
+* The below simple snippet demonstrates how you can run thousands of tasks concurrently with minimal resource usage.
+ */
+
+/*public class SampleVirtualThreads
+{
+	public static void main(String[] args)
+	{
+		try(var executor = Executors.newVirtualThreadPerTaskExecutor())
+		{
+			for(int i = 0; i < 1000; i++)
+			{
+				executor.submit(() -> System.out.println("Running task " + Thread.currentThread().getName()));
+			}
+		}
+	}
+}*/
+
+// With Thread Factory:
+
+public class SampleVirtualThreads
+{
+	public static void main(String[] args)
+	{
+		try(var executor = Executors.newThreadPerTaskExecutor(
+				Thread.ofVirtual().name("virtual-thread-", 0).factory()))
+		{
+			for(int i = 0; i < 10; i++)
+			{
+				final int taskNumber = i;
+				executor.submit(() ->
+				{
+					System.out.println("Running task " + taskNumber + " on " + Thread.currentThread().getName());
+				});
+			}
+		}
+	}
+}
