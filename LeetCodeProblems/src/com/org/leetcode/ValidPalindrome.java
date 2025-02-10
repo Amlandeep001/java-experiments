d59d8917ca84deddb795617540de4338f8793da9
Amlan Deep Nandi <99658242+Amlandeep001@users.noreply.github.com>
2025-02-11 02:01:57 +0530

    Problem

--- LeetCodeProblems/src/com/org/leetcode/ValidPalindrome.java
+++ LeetCodeProblems/src/com/org/leetcode/ValidPalindrome.java
@@ -4,27 +4,24 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * @author Amlan
+ * Link: https://leetcode.com/problems/valid-palindrome/submissions
+ */
 public class ValidPalindrome
 {
-	public boolean isPalindrome(String s)
-	{
-		List<String> list = Arrays.asList(s.split(""));
+    public boolean isPalindrome(String s)
+    {
+        List<String> list = Arrays.asList(s.split(""));
 
-		String ts = list.stream()
-				.map(e -> e.toLowerCase())
-				.filter(a -> a.chars().allMatch(Character::isLetterOrDigit))
-				.collect(Collectors.joining());
+        String ts = list.stream()
+                .map(e -> e.toLowerCase())
+                .filter(a -> a.chars().allMatch(Character::isLetterOrDigit))
+                .collect(Collectors.joining());
 
-		StringBuilder sb = new StringBuilder(ts).reverse();
+        StringBuilder sb = new StringBuilder(ts).reverse();
 
-		if(sb.toString().equals(ts))
-		{
-			return true;
-		}
-		else
-		{
-			return false;
-		}
-	}
+        return sb.toString().equals(ts);
+    }
 
 }
