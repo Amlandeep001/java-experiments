f2460c4b39f113923b2c90d159249ab3538ea281
Amlan Deep Nandi <amlandeep.nandi@gmail.com>
2025-02-26 02:40:57 +0530

    new changes

--- LeetCodeProblems/src/com/org/leetcode/ArrayIntersection.java
+++ LeetCodeProblems/src/com/org/leetcode/ArrayIntersection.java
@@ -3,24 +3,27 @@ package com.org.leetcode;
 import java.util.Arrays;
 import java.util.LinkedList;
 
+/**
+ * https://leetcode.com/problems/intersection-of-two-arrays/description/
+ */
 public class ArrayIntersection
 {
-	public int[] intersection(int[] nums1, int[] nums2)
-	{
-		return Arrays.stream(nums1)
-				.boxed()
-				.filter(new LinkedList<>(Arrays.stream(nums2).boxed().toList())::remove)
-				.mapToInt(Integer::intValue)
-				.toArray();
-	}
+    public int[] intersection(int[] nums1, int[] nums2)
+    {
+        return Arrays.stream(nums1)
+                .boxed()
+                .filter(new LinkedList<>(Arrays.stream(nums2).boxed().toList())::remove)
+                .mapToInt(Integer::intValue)
+                .toArray();
+    }
 
-	public static void main(String[] args)
-	{
-		int[] nums1 = {1, 2, 2, 3, 3, 4, 5};
-		int[] nums2 = {1, 2, 2, 3};
+    public static void main(String[] args)
+    {
+        int[] nums1 = {1, 2, 2, 3, 3, 4, 5};
+        int[] nums2 = {1, 2, 2, 3};
 
-		ArrayIntersection ai = new ArrayIntersection();
-		System.out.println(Arrays.toString(ai.intersection(nums1, nums2)));
-	}
+        ArrayIntersection ai = new ArrayIntersection();
+        System.out.println(Arrays.toString(ai.intersection(nums1, nums2)));
+    }
 
 }
