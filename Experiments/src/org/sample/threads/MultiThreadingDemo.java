53e2208dca3955dfdca3a34ae14cd024ffc8660c
Amlan Deep Nandi <99658242+Amlandeep001@users.noreply.github.com>
2025-03-01 15:09:15 +0530

    Changes

--- Experiments/src/org/sample/threads/MultiThreadingDemo.java
+++ Experiments/src/org/sample/threads/MultiThreadingDemo.java
@@ -18,18 +18,21 @@ package org.sample.threads;
  }
 }*/
 
-class MultithreadingDemo implements Runnable {
-    public void run()
-    {
-        try {
-            // Displaying the thread that is running
-            System.out.println(
-                "Thread " + Thread.currentThread().getId()
-                + " is running");
-        }
-        catch (Exception e) {
-            // Throwing an exception
-            System.out.println("Exception is caught");
-        }
-    }
+class MultithreadingDemo implements Runnable
+{
+	public void run()
+	{
+		try
+		{
+			// Displaying the thread that is running
+			System.out.println(
+					"Thread " + Thread.currentThread().threadId()
+							+ " is running");
+		}
+		catch(Exception e)
+		{
+			// Throwing an exception
+			System.out.println("Exception is caught");
+		}
+	}
 }
